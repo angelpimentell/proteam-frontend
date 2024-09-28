729e66d0e7cabda2af8651396e2b070dbeba3e4a
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-09-28 07:01:37 -0400

    Cahnge title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard Page | NextAdmin - Next.js Dashboard Kit",
+    "ProTeam - Project Management | NextAdmin - Next.js Dashboard Kit",
   description: "This is Next.js Home page for NextAdmin Dashboard Kit",
 };
 
